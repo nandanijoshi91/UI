0863a330616f3bd6657f60dd840d0c17319061c2
Nandani Joshi <joshinandani59@gmail.com>
2024-07-18 02:11:33 +0530

    create side bar

--- text.js
+++ text.js
@@ -1,24 +1,16 @@
-function updateDateTime() 
-{
+function updateDateTime() {
     let currentDate = new Date();
-
-  
-    let year = currentDate.getFullYear();
-    let month = currentDate.getMonth() + 1; 
-    let day = currentDate.getDate();
-
     let hours = currentDate.getHours();
     let minutes = currentDate.getMinutes();
-    let seconds = currentDate.getSeconds();
-
-    let formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
-    let formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-
-    document.getElementById('date-time').textContent = ` ${formattedDate} | ${formattedTime}`;
+    
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; 
+    
+    let formattedDate = "July 24 2024";
+    let formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}${ampm}`;
+    
+    document.getElementById('date-time').textContent = `${formattedDate} | ${formattedTime}`;
 }
-
 setInterval(updateDateTime, 1000);
-
-
 updateDateTime();
-
